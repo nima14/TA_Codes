90d6f5516068d1555e75d11c994c0a175eb5b5dc
Nima <nimataghidoost@gmail.com>
2021-02-23 16:49:20 +0330

    Sampling_Distribution is added.

---
+++ Sampling_Distirbution.R
@@ -0,0 +1,40 @@
+#Main Distribution
+set.seed(1234)
+
+n <-100000
+
+df <- data.frame(
+  x=rnorm(n, mean=180, sd=5)
+)
+
+
+
+ggplot(df, aes(x=x)) + 
+  geom_histogram(bins=20,color="black", fill="cyan")
+
+
+mean_df= mean(df$x)
+var_df= var(df$x)
+
+#Sampling Distribution
+
+set.seed(25)
+
+n_sample <- 200
+sampling <- replicate(1000,sample(df$x,n_sample))
+
+sampling_distribution <-  data.frame(
+      x=colMeans(sampling)
+      
+)
+
+
+
+ggplot(sampling_distribution, aes(x=x)) + 
+  geom_histogram(bins=20,color="black", fill="cyan")
+
+
+mean_sample= mean(sampling_distribution$x)
+var_sample=  var(sampling_distribution$x)
+
+var_df/n_sample
