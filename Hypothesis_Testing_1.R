58030ede54e23c5a7ab6237310184c65e4a1a184
Nima Taghidoost <N.taghidoost@Son.ir>
2021-04-12 14:49:09 +0430

    Hypothesis_Testing is changed after the lecture.

--- Hypothesis_Testing_1.R
+++ Hypothesis_Testing_1.R
@@ -3,8 +3,7 @@ data(women)
 women$height_cm= women$height * 2.54
 n <- 15
 mu_0 <- 168
-
-#mu_0 <- 171
+#mu_0 <- 175
 
 mean_sample <- mean(women$height_cm)
 S <- sd(women$height_cm)
@@ -18,30 +17,28 @@ t.test(women$height_cm,mu=mu_0,alternative = 'two.sided',conf.level = 0.95)
 
 
 t0 <- (mean_sample - mu_0)/ S_sample
-
-p_value <- 2*pt(-abs(t0),n-1)
-
-conf_int <- c(mean_sample - qt(0.975,n-1)*S_sample,mean_sample + qt(0.975,n-1)*S_sample)
-
 acc_range <- c(- qt(0.975,n-1), qt(0.975,n-1))
 
 acc_range_for_Xbar <- c(mu_0 - qt(0.975,n-1)*S_sample,mu_0 + qt(0.975,n-1)*S_sample)
 
+
+p_value <- 2*pt(-abs(t0),n-1)
+#conf_int <- c(mean_sample - qt(0.975,n-1)*S_sample,mean_sample + qt(0.975,n-1)*S_sample)
 #----------------------------------------------
 #one.sided
 
 t.test(women$height_cm,mu=mu_0,alternative = 'less',conf.level = 0.95)
 
 t0 <- (mean_sample - mu_0)/ S_sample
-
-p_value <- pt(t0,n-1)
-
-conf_int <- c(-Inf,mean_sample + qt(0.95,n-1)*S_sample)
-
 acc_range <- c(- qt(0.95,n-1), Inf)
+
 
 acc_range_for_Xbar <- c(mu_0 - qt(0.95,n-1)*S_sample,Inf)
 
+
+
+p_value <- pt(t0,n-1)
+#conf_int <- c(-Inf,mean_sample + qt(0.95,n-1)*S_sample)
 
 #---------------------------------------------
 
@@ -49,24 +46,25 @@ acc_range_for_Xbar <- c(mu_0 - qt(0.95,n-1)*S_sample,Inf)
 
 sd_pop <- 11
 sd_sample <- sd_pop/sqrt(n)
+
+
 z0 <- (mean_sample - mu_0)/ sd_sample
-
-
-p_value <- 2*pnorm(-abs(z0))
-
-conf_int <- c(mean_sample - qnorm(0.975)*S_sample,mean_sample + qnorm(0.975)*S_sample)
-
 acc_range <- c(- qnorm(0.975), qnorm(0.975))
+
 
 acc_range_for_Xbar <- c(mu_0 - qnorm(0.975)*S_sample,mu_0 + qnorm(0.975)*S_sample)
 
 
+p_value <- 2*pnorm(-abs(z0))
+
+#conf_int <- c(mean_sample - qnorm(0.975)*S_sample,mean_sample + qnorm(0.975)*S_sample)
 
 #---------------------------------
 
 #Chi2 Test for variance
 
 #One.sided
+
 var_0 <- 121
 #var_0 <- 70
 S2 <- var(women$height_cm)
@@ -75,35 +73,36 @@ library(EnvStats)
 
 varTest(women$height_cm,sigma.squared =var_0,alternative = 'greater',conf.level = 0.95)
 
+
 ch_0 <- (n-1)*S2/var_0
+acc_range <- c(0,qchisq(0.95,n-1))
+
+
+acc_range_for_S2 <- c(0,qchisq(0.95,n-1)*var_0/(n-1))
 
 
 p_value <- 1- pchisq(ch_0,n-1) 
 
-
-conf_int <- c(S2*(n-1)/qchisq(0.95,n-1),Inf)
-
-acc_range <- c(0,qchisq(0.95,n-1))
-
-acc_range_for_Xbar <- c(0,qchisq(0.95,n-1)*var_0/(n-1))
-
+#conf_int <- c(S2*(n-1)/qchisq(0.95,n-1),Inf)
 #---------------------------------------------
 
 #two.sided
-var_0 <- 121
+#var_0 <- 121
 var_0 <- 60
 S2 <- var(women$height_cm)
 
 varTest(women$height_cm,sigma.squared =var_0,alternative = 'two.sided',conf.level = 0.95)
 
+
+
+
 ch_0 <- (n-1)*S2/var_0
+acc_range <- c(qchisq(0.025,n-1),qchisq(0.975,n-1))
+
+
+acc_range_for_S2 <- c(qchisq(0.025,n-1)*var_0/(n-1),qchisq(0.975,n-1)*var_0/(n-1))
+
 
 
 p_value <- 2*(1- pchisq(ch_0,n-1) ) 
-
-
-conf_int <- c(S2*(n-1)/qchisq(0.975,n-1),S2*(n-1)/qchisq(0.025,n-1))
-
-acc_range <- c(qchisq(0.025,n-1),qchisq(0.975,n-1))
-
-acc_range_for_Xbar <- c(qchisq(0.025,n-1)*var_0/(n-1),qchisq(0.975,n-1)*var_0/(n-1))
+#conf_int <- c(S2*(n-1)/qchisq(0.975,n-1),S2*(n-1)/qchisq(0.025,n-1))
